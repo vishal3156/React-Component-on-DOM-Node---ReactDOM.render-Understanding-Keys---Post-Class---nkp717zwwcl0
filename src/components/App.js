c3dd33971c63ad678a147178e9d4d08157007346
Vishal Suman <119152809+vishal3156@users.noreply.github.com>
2023-04-03 16:41:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import '../styles/App.css';
 
-function App{
+const App = () => {
     	return(
+            <div>
         <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+            </div>
     );
   }
 export default App;
